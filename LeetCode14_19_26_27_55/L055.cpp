bca8b939ff72eefd70f29fb0db40981cddb31755
Joe <472167879@qq.com>
2016-06-21 14:04:03 +0800

    Signed-off-by: Joe <472167879@qq.com>

--- LeetCode14_19_26_27_55/L055.cpp
+++ LeetCode14_19_26_27_55/L055.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-    	int sz = s.size(), pz = p.size();
-    	if (sz == 0){
-    		if (pz == 0) return true;
-    		bool ret = false;
-    		if (pz % 2 == 0) {
-    			ret = true;
-    			for (int i = 1; i < pz; i+=2){
-    				if (p[i] != '*'){
-    					ret = false;
-    					break;
-    				}
-    			}
-    		}
-    		return ret;
-    	}
-    	if (pz == 0) return false;
-    	if (p[pz - 1] == '.' || p[pz - 1] == s[sz - 1])
-    		return isMatch(s.substr(0, sz - 1), p.substr(0, pz - 1));
-    	else if (p[pz - 1] == '*'){
-    		if (p[pz - 2] == '.' || p[pz - 2] == s[sz - 1])
-    			return isMatch(s.substr(0, sz), p.substr(0, pz - 2)) || isMatch(s.substr(0, sz - 1), p.substr(0, pz - 2)) || isMatch(s.substr(0, sz - 1), p.substr(0, pz));
-    		else
-    			return isMatch(s.substr(0, sz), p.substr(0, pz - 2));
-    	}
-    	else
-    		return false;
+    bool canJump(vector<int>& nums) {
+        int nz=nums.size();
+        bool ok[100000];
+        ok[0]=true;
+        for(int i=1;i<nz;i++){
+            bool f=false;
+            for(int j=i-1;j>=0;j--){
+                if(ok[j]&&nums[j]+j>=i){
+                    f=true;
+                    break;
+                }
+            }
+            ok[i]=f;
+        }
+        
+        return ok[nz-1];
     }
 };
