00be1d40da56abc645ef367a182205440f07a32f
Joe <472167879@qq.com>
2016-09-07 20:42:23 +0800

    Signed-off-by: Joe <472167879@qq.com>

--- LeetCode14_19_26_27_46_55/L046.cpp
+++ LeetCode14_19_26_27_46_55/L046.cpp
@@ -23,3 +23,44 @@ public:
         return ans;
     }
 };
+
+//P(n,k)
+class Solution {
+public:
+    void dfsp(vector<int> &v, vector<bool> &mk, int cnt, int k, vector<vector<int>> &ans){
+    	int sz = v.size();
+    	if (k > sz){ printf("Error!\n"); return; }
+    	if (cnt == k){
+    		vector<int> vr(k);
+    		for (int i = 0; i < k; i++){
+    			vr[i] = v[i];
+    		}
+    		ans.push_back(vr);
+    	}
+    	for (int i = 1; i <= sz; i++){
+    		if (!mk[i]){
+    			mk[i] = 1;
+    			v[cnt] = i;
+    			dfsp(v, mk, cnt + 1, k, ans);
+    			mk[i] = 0;
+    		}
+    	}
+    }
+    vector<vector<int>> permute(vector<int>& nums, int k) {
+    	int sz = nums.size();//,k=sz;
+    	sort(nums.begin(), nums.end());
+    	vector<int> vt(sz);
+    	vector<bool> mk(sz+1, 0);
+    	vector<vector<int>> ans;
+    	dfsp(vt, mk, 0, k, ans);
+    
+    	int rsz = ans.size();
+    	vector<vector<int>> ret=ans;
+    	for (int i = 0; i < rsz; i++){
+    		for (int j = 0; j < k; j++){
+    			ret[i][j] = nums[ans[i][j]-1];
+    		}
+    	}
+    	return ret;
+    }
+};
